8bdcf058b7aaf4082a10e25a1c2293b5974dd475
Guardevour <tachankadt@gmail.com>
2024-04-19 11:13:27 +0300

    Initial

---
+++ src/main/kotlin/org/guardevour/Application.kt
@@ -0,0 +1,34 @@
+package org.guardevour
+
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.guardevour.plugins.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureDatabases()
+    configureRouting()
+}
+
+fun Application.configureRouting(){
+    routing {
+        staticResources("/content", "mycontent")
+
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        get("/test1") {
+            val text = "<h1>Hello From Ktor</h1>"
+            val type = ContentType.parse("text/html")
+            call.respondText(text, type)
+        }
+    }
+}
